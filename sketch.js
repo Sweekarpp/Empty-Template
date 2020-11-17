b0191f22b9a5b3c42b2acafe868b045ca971dff1
Sweekarpp <sweekarpprabhu@gmail.com>
2020-11-17 15:30:00 +0530

    added ground

--- sketch.js
+++ sketch.js
@@ -3,15 +3,15 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-
+var ground;
 function preload()
 {
-	
+
 }
 
 function setup() {
 	createCanvas(800, 700);
-
+ground = createSprite(400,690,800,20);
 
 	engine = Engine.create();
 	world = engine.world;
@@ -20,17 +20,14 @@ function setup() {
 
 
 	Engine.run(engine);
-  
+
 }
 
 
 function draw() {
   rectMode(CENTER);
   background(0);
-  
+
   drawSprites();
- 
+
 }
-
-
-
